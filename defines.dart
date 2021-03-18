7134df1f4ccfe0f6ebe0394c35a4ed0d389352d6
YinG17 <simbulanja.17@gmail.com>
2021-03-18 10:14:21 +0800

    remove unused codes

--- defines.dart
+++ defines.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class Space {
-  static const double xxs = 4;
-  static const double xs = 8;
-  static const double xsm = 12;
-  static const double sm = 16;
-  static const double md = 24;
-  static const double lg = 32;
-  static const double xl = 40;
-  static const double xxl = 56;
-}
+// class Space {
+//   static const double xxs = 4;
+//   static const double xs = 8;
+//   static const double xsm = 12;
+//   static const double sm = 16;
+//   static const double md = 24;
+//   static const double lg = 32;
+//   static const double xl = 40;
+//   static const double xxl = 56;
+// }
 
-const stylePostTitle = TextStyle(
-  fontSize: 20,
-  color: Colors.blueGrey,
-  fontWeight: FontWeight.w400,
-  // fontFamily: '',
-);
+// const stylePostTitle = TextStyle(
+//   fontSize: 20,
+//   color: Colors.blueGrey,
+//   fontWeight: FontWeight.w400,
+//   // fontFamily: '',
+// );
 
-const styleHintText = TextStyle(fontSize: Space.xsm, color: Colors.grey);
+// const styleHintText = TextStyle(fontSize: Space.xsm, color: Colors.grey);
